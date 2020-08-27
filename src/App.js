87c91866a7e8d3157e6d80738065fe37b212398e
Alexandra Lim <alexandra.lim@gmail.com>
2020-08-27 00:28:24 -0400

    remove commented out code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-    // return React.createElement("div", {}, [
-    //     React.createElement("h1", {}, "Adopt Me!"),
-    //     React.createElement(Pet, { name: 'Sherlock', animal: 'Cat', breed: 'Domestic Shorthair' }),
-    //     React.createElement(Pet, { name: 'Lola', animal: 'Cat', breed: 'Siamese' }),
-    //     React.createElement(Pet, { name: 'Kali', animal: 'Cat', breed: 'Calico' })
-    // ]);
     return (
         <div>
             <h1>Adopt Me!</h1>
